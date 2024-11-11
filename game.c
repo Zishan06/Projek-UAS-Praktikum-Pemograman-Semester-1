3ec5985bc516963fa82d28f9f56b1393fd2f844a
M. Rayan Zishan <147892080+Zishan06@users.noreply.github.com>
2024-11-11 19:35:01 +0700

    login.c

--- game.c
+++ game.c
@@ -1,7 +1,43 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main(){
-    printf("Hello Rayyan");
+    struct pengguna{
+        char username[10];
+        char pass[10];
+    };
+
+    void IniUntukBuatFileBinYagesya(const char *namafile, struct pengguna user){
+        FILE *file = fopen(namafile, "ab");
+        if(file == NULL){
+            perror("Error pulak bah");
+            exit(1);
+        }
+        fwrite(&user, sizeof(struct pengguna), 1, file);
+
+        fclose(file);
+        printf("sername sudah disimpan, have fun!");
+    }
+
+    int main(){
+        struct pengguna penggunabaru;
+        printf("Selamat datang di game paling gacor\n");
+        printf("Silahkan buat akun dulu\n");
+        
+        printf("Masukkan username : ");
+        fgets(penggunabaru.username, sizeof(penggunabaru.username), stdin);
+        penggunabaru.username[strcspn(penggunabaru.username, "\n")] = '\0';
+
+        printf("Masukkan password : ");
+        fgets(penggunabaru.pass, sizeof(penggunabaru.pass), stdin);
+        penggunabaru.pass[strcspn(penggunabaru.pass, "\n")] = '\0';
+
+        IniUntukBuatFileBinYagesya("pengguna.bin", penggunabaru);
+
+
+        
+    
+
 
     return 0;
 }
