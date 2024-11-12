37c0917b8dfc2d7bc11ebac77227aaa9c3498503
Abdullah Asy-Syifawi <abdul.asy07@gmail.com>
2024-11-13 01:09:07 +0700

    p

--- main.c
+++ main.c
@@ -5,15 +5,14 @@
     struct pengguna{
         char username[10];
         char pass[10];
+        int score;
     };
 
     void registrationSystem(){
         struct pengguna penggunabaru;
-        
-        char buffer[255];
-            
-        printf("Selamat datang di game paling gacor maxwinn777\n");
-        printf("Silahkan buat akun dulu\n");
+        char confirmPassword[10];
+    
+        printf("Selamat datang di game paling gacor maxwinn777\nKetik Username dan password untuk mendaftar\n");
         
         printf("Masukkan username : ");
         fgets(penggunabaru.username, sizeof(penggunabaru.username), stdin);
@@ -21,23 +20,28 @@
             
         printf("Masukkan password : ");
         fgets(penggunabaru.pass, sizeof(penggunabaru.pass), stdin);
+        penggunabaru.username[strcspn(penggunabaru.username, "\n")] = '\0';
 
         FILE *regis = fopen("database/login.bin", "ab");
         if(regis == NULL){
                 perror("Error ges");
                 exit(1);
         }
-        snprintf(buffer, 255, "%s#%s", penggunabaru.username, penggunabaru.pass);
-        fwrite(buffer, 1, strlen(buffer), regis);
+        fprintf(regis, "%s#%s=0\n", penggunabaru.username, penggunabaru.pass);
         fclose(regis);
     }
 
     int main(int argc, char *argv[]){
+        
         if(strcmp(argv[1], "register")==0 && argc == 2) {
             registrationSystem();
             
         } else if(strcmp(argv[1], "login")==0 && argc==4) {
+            struct pengguna users[10];
+            char binFile[100];
             FILE *file = fopen("database/login.bin", "rb");
+            fgets(binFile, sizeof(binFile), file);
+            
         }
         else {
             printf("Format CLA tidak sesuai, coba:\n-> ./main register\n-> ./main login (username) (password)\n");
