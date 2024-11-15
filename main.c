c20b11ecf7a05888c0d13b740d3e34e89fe0215e
Syifatya <syifaathiyyaa@gmail.com>
2024-11-15 11:12:19 +0700

    Update soal
    nambah 10 soal ygy

--- main.c
+++ main.c
@@ -107,6 +107,7 @@
     }
 
     void soalQuiz(double *skor){
+        
         const char *soal[] = {
             "Apa nama lautan terbesar di dunia?",
             "Planet terdekat dengan matahari adalah?",
@@ -117,7 +118,17 @@
             "Apa mata uang resmi jepang?",
             "Negara apa yang disebut dengan 'Negari Tirai Bambu'?",
             "Apa bahasa resmi di brazil?",
-            "Berapa warna yang ada pada bendera italia?"};
+            "Berapa warna yang ada pada bendera italia?",
+            "apabila nun mati atau tanwin bertemu dengan huruf fa maka hukumnya?",
+            "di bawah ini yang termasuk sifat mustahil nabi adalah?",
+            "ada berapa sifat wajib Allah?",
+            "siapa nama dosen kece kelas b?",
+            "syifa ngecrushin doi selama 2 tahun, ternyata doi udah punya pacar, tapi ga dipost aja. tebak berapa hts syifa sekarang?",
+            "7 ekor buaya dikali 3, berapa total buaya sekarang?",
+            "5 x 4 + 3 : 100 + kamu sama yang lain = ?",
+            "berapa banyak rukun shalat?",
+            "di tahan sakit, di keluarin malu, apakah gerangan?",
+            "berapa banyak rukun islam?"};
 
         const char *pilihan[][4] = {
             {"Samudra Atlantik", "Samudra Arktik", "Samudra Pasifik", "Samudra Hindia"},
@@ -129,9 +140,19 @@
             {"Yuan", "Won", "Yen", "Dong"},
             {"Jepang", "China", "Korea Selatan", "Vietnam"},
             {"Portugis", "Spanyol", "Inggris", "Prancis"},
-            {"3", "2", "4", "5"}};
+            {"3", "2", "4", "5"},
+            {"ikhfa", "iqlab", "mad asli", "saktah"},
+            {"sidik", "amanah", "kadzib", "tablig"},
+            {"15", "20", "1", "10"},
+            {"pak mahyus", "pak taufiq", "s14p4 y4ch?", "pak alim<3"},
+            {"0 lah bjir stay halal!!", "keknya si 4 yah", "1 fakultas sie", "otw cari tuh"},
+            {"21", "4", "10", "15"},
+            {"aku yo ngoding lah ;p", "10+ hts", "23", "aku ma siapa? </3 :("},
+            {"10", "11", "9", "13" },
+            {"kentut anjay", "rasa cintaku padanya", "jawab nomor 2", "kentut tapi ga bunyi"},
+            {"6", "5","2", "3"}};
 
-        const int jawabanBenar[] = {2, 1, 0, 2, 3, 1, 2, 1, 0, 0};
+        const int jawabanBenar[] = {2, 1, 0, 2, 3, 1, 2, 1, 0, 0, 0, 2, 1, 3, 0, 2, 0, 3, 0, 1};
 
         int jumlahSoal = sizeof(soal) / sizeof(soal[0]);
         int soalTampil[jumlahSoal];
